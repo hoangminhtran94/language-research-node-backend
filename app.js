19d8f3bedf706a3047d566e0b71be87b26967c71
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-15 01:20:01 -0400

    update read-csv

--- app.js
+++ app.js
@@ -9,76 +9,14 @@
  */
 const fs = require("fs");
 const express = require("express");
-
 const app = express();
+const { readCSV } = require("./utils/read-csv");
 
+readCSV("./32100260.csv").then((res) => console.log(res));
 /**
  * Use nodejs file system to read the csv file
  * * Student name: Minh Hoang Tran
  */
-fs.readFile("./32100260.csv", "utf8", (err, contents) => {
-  /**
-   * If failed to read the file, log the error and return
-   */
-  if (err) {
-    console.error(err);
-    return;
-  }
-  /**
-   * Create an array to store all record data
-   */
-
-  const finalData = [];
-
-  /**
-   * reate an array of strings by split the contents by the special value "\n"  (new line)
-   */
-  const rows = contents.split("\n");
-
-  /**
-   * splice the first row from rows, which contains table labels. Remove the `"` and split the string into
-   * an array of string using ","
-   */
-  const labels = rows.splice(0, 1)[0].replace(/"/g, "").split(",");
-
-  /**
-   * Loop through all the row data, remove the `"` and split value data using ","
-   */
-  rows.forEach((rawRowData) => {
-    /**
-     *  Remove the `"`  in raw data
-     */
-    const rowData = rawRowData.replace(/"/g, "");
-    /**
-     *  split raw string into an array of string using ","
-     */
-    const cells = rowData.split(",");
-
-    /**
-     *  Create an empty record data object
-     */
-    let dataObject = {};
-
-    /**
-     * Loop through all cells, for each cell, pick an label from the label array and match with the value of the cell using
-     * Javascript spread operator
-     *  Student name: Minh Hoang Tran
-     */
-    cells.forEach((cell, index) => {
-      dataObject = { ...dataObject, [labels[index]]: cell };
-    });
-
-    /**
-     * Push the record data into the record list
-     */
-    finalData.push(dataObject);
-  });
-  /**
-   * Display the result in the terminal
-   */
-  console.log(finalData);
-  console.log("Deleloped by Minh Hoang Tran");
-});
 
 /**
  * Student name: Minh Hoang Tran
---
+++ utils/read-csv.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+exports.readCSV = async (filepath) => {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(filepath, "utf8", (err, contents) => {
+      /**
+       * If failed to read the file, log the error and return
+       */
+      if (err) {
+        console.error(err);
+        return resolve(null);
+      }
+      /**
+       * Create an array to store all record data
+       */
+
+      const finalData = [];
+
+      /**
+       * reate an array of strings by split the contents by the special value "\n"  (new line)
+       */
+      const rows = contents.split("\n");
+
+      /**
+       * splice the first row from rows, which contains table labels. Remove the `"` and split the string into
+       * an array of string using ","
+       */
+      const labels = rows.splice(0, 1)[0].replace(/"/g, "").split(",");
+
+      /**
+       * Loop through all the row data, remove the `"` and split value data using ","
+       */
+      rows.forEach((rawRowData) => {
+        /**
+         *  Remove the `"`  in raw data
+         */
+        const rowData = rawRowData.replace(/"/g, "");
+        /**
+         *  split raw string into an array of string using ","
+         */
+        const cells = rowData.split(",");
+
+        /**
+         *  Create an empty record data object
+         */
+        let dataObject = {};
+
+        /**
+         * Loop through all cells, for each cell, pick an label from the label array and match with the value of the cell using
+         * Javascript spread operator
+         *  Student name: Minh Hoang Tran
+         */
+        cells.forEach((cell, index) => {
+          dataObject = { ...dataObject, [labels[index]]: cell };
+        });
+
+        /**
+         * Push the record data into the record list
+         */
+        finalData.push(dataObject);
+      });
+      /**
+       * Display the result in the terminal
+       */
+      console.log("Deleloped by Minh Hoang Tran");
+      resolve(finalData);
+    });
+  });
+
+  return await promise;
+};
