2de6e39b0fadac731538f3033e9c811498eda939
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-15 02:12:13 -0400

    update model, controller

--- controller/vegetable-controller.js
+++ controller/vegetable-controller.js
@@ -1,6 +1,19 @@
 const { readCSV } = require("../utils/read-csv");
+const VegetableRecord = require("./../models/vegetable-record");
 
 exports.getAll = async (req, res, next) => {
   const data = await readCSV("./32100260.csv");
   return res.json(data).status(201);
+};
+
+exports.addNewRecord = async (req, res, next) => {
+  const data = req.body;
+  const newRecord = new VegetableRecord({
+    geo: data.geo,
+    typeOfProduct: data.typeOfProduct,
+    vector: data,
+    vector,
+    cordinate: data.cordinate,
+    value: data.value,
+  });
 };
---
+++ models/vegetable-record.js
@@ -0,0 +1,22 @@
+class VegetableRecord {
+  REF_DATE = new Date(Date.now()).toLocaleDateString();
+  DGUID = "";
+  type_of_storage = "Cold and common storage";
+  UOM = "Tonnes";
+  UOM_ID = 288;
+  SCALAR_FACTOR = "unit";
+  SCALAR_ID = 0;
+  STATUS = "";
+  SYMBOL = "";
+  TERMINATED = "";
+  DECIMALS = 0;
+  constructor({ geo, typeOfProduct, vector, cordinate, value }) {
+    this.GEO = geo;
+    this.type_of_product = typeOfProduct;
+    this.VECTOR = "v" + vector;
+    this.COORDINATE = cordinate;
+    this.VALUE = value;
+  }
+}
+
+module.exports = VegetableRecord;
